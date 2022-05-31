911e8aa4636e4743672a2d5c11ea6198fb221fe3
GUIVZZ <guivazoliveira2002@gmail.com>
2022-05-31 20:54:00 -0300

    new

---
+++ firebase-db.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCWBnqT2ihGTImFvRagqCaE6vT8sCdYB4E",
+    authDomain: "filmes-atividade.firebaseapp.com",
+    databaseURL: "https://filmes-atividade-default-rtdb.firebaseio.com",
+    projectId: "filmes-atividade",
+    storageBucket: "filmes-atividade.appspot.com",
+    messagingSenderId: "620322745029",
+    appId: "1:620322745029:web:8b4db7bf6ff3e7fcbd9eac"
+  
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Get a reference to the database service
+const database = getDatabase(app);
+
+export default database
+
+
---
+++ server.js
@@ -0,0 +1,71 @@
+import express from "express"
+import cors from "cors"
+
+import database from "./firebase-db.js"
+import { ref, get, set } from "firebase/database"
+
+// iniciando o express
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Rota raiz direciona para a pasta front
+app.use("/", express.static("./front"))
+
+
+app.get('/filmes', function (req, res) {
+    // Pega uma referência para o caminho /bandas
+    let filmesRef = ref(database, "/filmes")
+    get(filmesRef).then((snap) => {
+        let listaFilmes = snap.val()
+        console.log("listaFilmes", listaFilmes);
+        return res.status(200).json(listaFilmes)
+    })
+})
+
+app.post('/filmes', function (req, res) {
+    console.log("recebi requisição POST com body: ",req.body);
+    //pega os dados enviados na requisição
+    let dados = req.body
+    let id = dados.id
+    // Pega uma referência para o caminho /bandas/<ID>
+    let novaFilmeRef = ref(database, "/filmes/" + id)
+    // Adiciona dados no firebase no caminho /bandas/<ID>
+    set(novaFilmeRef, dados).then(() => {
+        console.log("Adicionado")
+        return res.status(200).json(dados)
+    })
+})
+
+app.put('/filmes/:id', function (req, res) {
+    console.log("recebi requisição PUT com body: ",req.body);
+    //pega os dados enviados na requisição
+    let dados = req.body
+    let id = req.params.id
+    // Pega uma referência para o caminho /bandas/<ID>
+    let novaFilmeRef = ref(database, "/filmes/" + id)
+    // Adiciona dados no firebase no caminho /bandas/<ID>
+    set(novaFilmeRef, dados).then(() => {
+        console.log("Adicionado")
+        return res.status(200).json(dados)
+    })
+})
+
+app.delete('/filmes/:id', function (req, res) {
+    console.log("recebi requisição POST com body: ",req.body);
+    //pega os dados enviados na requisição
+    let dados = req.body
+    let id = req.params.id
+    // Pega uma referência para o caminho /bandas/<ID>
+    let novaFilmeRef = ref(database, "/filmes/" + id)
+    // Remove /bandas/<ID>
+    set(novaFilmeRef, null).then(() => {
+        console.log("Excluido")
+        return res.status(200).json(dados)
+    })
+})
+
+app.listen(3000, function () {
+    console.log("Servidor rodando na porta 3000 ...");
+})
